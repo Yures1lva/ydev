3be439bb96cc119c89b55474fde017168aa7dd97
Yures1lva <yure.siilva9@gmail.com>
2023-11-17 16:03:08 -0300

    up: theme initial confgurations

---
+++ src/styles/global.tsx
@@ -0,0 +1,20 @@
+import {createGlobalStyle} from 'styled-components'
+
+
+export const lightTheme ={
+    'background': '#fff',
+    'primary': '#2B64D8',
+    'primary-dark':'#0E3C9A',
+    'primary-light':'#DEE5F3',
+
+
+}
+
+export const GlobalStyle = createGlobalStyle`
+  *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+`
